ea462cf0d1041304d05d8ac9e9832dad42be59f3
Vindhya Shree <140383596+vindhyashre@users.noreply.github.com>
2024-12-28 16:49:27 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,77 @@
+// npm init : package.json -- This is a node project.
+// npm i express : expressJs package install hogya. -- project came to know that we are using express
+// We finally use express
+
+const express = require("express");
+const mongoose = require("mongoose");
+const JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
+const User = require("./models/User");
+const authRoutes = require("./routes/auth");
+const songRoutes = require("./routes/song");
+const playlistRoutes = require("./routes/playlist");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = 8080;
+
+app.use(cors());
+app.use(express.json());
+
+// connect mongodb to our node app.
+// mongoose.connect() takes 2 arguments : 1. Which db to connect to (db url), 2. 2. Connection options
+mongoose
+  .connect(
+    //
+
+    "mongodb+srv://admin:" +
+      process.env.MONGO_PASSWORD +
+      "@spotify-clone.ftmhg.mongodb.net/?retryWrites=true&w=majority&appName=spotify-clone",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((x) => {
+    console.log("Connected to Mongo!");
+  })
+  .catch((err) => {
+    console.log("Error while connecting to Mongo");
+  });
+
+// setup passport-jwt
+let opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "thisKeyIsSupposedToBeSecret";
+passport.use(
+  new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ _id: jwt_payload.identifier }, function (err, user) {
+      // done(error, doesTheUserExist)
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+        // or you could create a new account
+      }
+    });
+  })
+);
+
+// API : GET type : / : return text "Hello world"
+app.get("/", (req, res) => {
+  // req contains all data for the request
+  // res contains all data for the response
+  res.send("Hello World");
+});
+app.use("/auth", authRoutes);
+app.use("/song", songRoutes);
+app.use("/playlist", playlistRoutes);
+
+// Now we want to tell express that our server will run on localhost:8000
+app.listen(port, () => {
+  console.log("App is running on port " + port);
+});
